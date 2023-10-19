32b86a72baa3385cb20e3bbdcddd82425fa9c76d
BSJ <sjhj000121@gmail.com>
2023-10-19 17:43:17 +0900

    아무것도안함

--- popol2/src/App.js
+++ popol2/src/App.js
@@ -4,6 +4,7 @@ import {Routes, Route} from 'react-router-dom';
 import Header from './routes/Header';
 import Footer from './routes/Footer';
 import Main from './routes/Main';
+import Playlist from './routes/Playlist';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Header/>
       <Routes>
         <Route path='/' element={<Main></Main>}></Route>
+        <Route path='/playlist' element={<Playlist></Playlist>}></Route>
       </Routes>
       <Footer/>
     </div>
