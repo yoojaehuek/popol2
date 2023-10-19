da6c9e1153b6a053a2a3c26aae062f7e90ffec41
chajeongheon <jeongheon1127@gmail.com>
2023-10-19 17:46:07 +0900

    Mypage

---
+++ popol2/src/routes/Mypage.js
@@ -0,0 +1,13 @@
+import React from "react";
+import '../scss/Mypage.scss';
+
+const Mypage = () => {
+  return(
+    <>
+      <h1>나다.</h1>
+      <p>dssadsa</p>
+    </>
+  )
+}
+
+export default Mypage;
