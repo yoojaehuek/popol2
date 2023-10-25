4eb74e39aa612009efb7aaf3ce75ea7124a9e2a5
김지환 <k1004902@gmail.com>
2023-10-25 23:01:42 +0900

    음악등록 성공

--- popol2/src/App.js
+++ popol2/src/App.js
@@ -7,6 +7,7 @@ import Main from './routes/Main';
 import Playlist from './routes/Playlist';
 import Mypage from './routes/Mypage';
 import Musics from './routes/Musics';
+import Uploader from './routes/Uploader';
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
         <Route path='/playlist' element={<Playlist></Playlist>}></Route>
         <Route path='/mypage' element={<Mypage></Mypage>}></Route>
         <Route path='/music' element={<Musics></Musics>}></Route>
+        <Route path='/uploader' element={<Uploader></Uploader>}></Route>
       </Routes>
       <Footer/>
     </div>
--- routes/musics.js
+++ routes/musics.js
@@ -14,4 +14,23 @@ router.route('/')
       next(err);
     }
   })
+  .post(async (req, res, next) => {
+    try {
+      console.log(req.body)
+      const music = await Music.create({ //Product모델에 INSERT INTO
+        imageUrl: req.body.imageUrl,
+        musicUrl: req.body.musicUrl,
+        name: req.body.name,
+        kind: req.body.kind,
+        singer: req.body.singer,
+        composer: req.body.composer,
+        lyricist: req.body.lyricist,
+        lyrics: req.body.lyrics,
+      })
+      console.log(music);
+      res.status(201).end();
+    }catch{
+
+    }
+  })
 module.exports = router;
