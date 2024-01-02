0c4224e226a35de501be5b8991eef5130811cf97
BSJ <sjhj000121@gmail.com>
2024-01-02 09:29:04 +0900

    123

--- popol2/src/routes/Playlist.js
+++ popol2/src/routes/Playlist.js
@@ -1,13 +1,49 @@
-import React from "react";
+import React, { Component } from "react";
 import '../scss/Playlist.scss';
 
-const Playlist = () => {
-  return(
-    <>
-      <h1>조졌노</h1>
-      <p>dssadsa</p>
-    </>
-  )
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      selectedButton: 'playlist',
+    };
+  }
+
+  handleButtonClick = (buttonName) => {
+    this.setState({ selectedButton: buttonName });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="sidebar">
+          <button
+            onClick={() => this.handleButtonClick('MyPage')}
+            className={this.state.selectedButton === 'MyPage' ? 'active' : ''}
+          >
+            MyPage
+          </button>
+          <button
+            onClick={() => this.handleButtonClick('Playlist')}
+            className={this.state.selectedButton === 'Playlist' ? 'active' : ''}
+          >
+            Playlist
+          </button>
+          <button
+            onClick={() => this.handleButtonClick('New Playlist')}
+            className={this.state.selectedButton === 'New Playlist' ? 'active' : ''}
+          >
+            New Playlist
+          </button>
+        </div>
+        <div className="content">
+          {this.state.selectedButton === 'MyPage' && <h2>마이페이지</h2>}
+          {this.state.selectedButton === 'Playlist' && <h2>플레이리스트</h2>}
+          {this.state.selectedButton === 'New Playlist' && <h2>새 플레이리스트</h2>}
+        </div>
+      </div>
+    );
+  }
 }
 
-export default Playlist;
+export default App;
